53b5e2976f8d132f593e1a5223d89d56d406c5ff
Sushmitha DT <41836325+Sushmitha-93@users.noreply.github.com>
2022-03-12 01:06:20 -0800

    authentication

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-echo  readfile("contacts.txt");
+// $x = readfile("contacts.txt");
+// echo json_decode($x, true);
+$contactsJSON =  file_get_contents("contacts.txt");
+
+$contacts = json_decode($contactsJSON);
+
+echo json_encode($contacts[0]); 
+
+
+//return ($contacts);
