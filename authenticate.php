6f77bee2da071188b488094375ab7ccf9cda1bdd
Sushmitha DT <41836325+Sushmitha-93@users.noreply.github.com>
2022-03-12 01:11:56 -0800

    Add files via upload

---
+++ authenticate.php
@@ -0,0 +1,11 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if($_POST['userid']=='admin' && $_POST['password']=="password")
+    echo("true");
+    else
+    echo("false");
+}
